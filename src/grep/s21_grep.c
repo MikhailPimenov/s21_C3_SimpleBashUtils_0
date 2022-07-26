d81135b1ff4aa9a5281308499a659bab55b3751a
Elvia Attack <elviaatt@oa-a2.msk.21-school.ru>
2022-07-27 00:42:56 +0300

    develop/grep - basic grep works

--- src/grep/s21_grep.c
+++ src/grep/s21_grep.c
@@ -11,6 +11,7 @@ enum Boolean {
 };
 
 typedef struct Patterns {
+    const char** words;                                                                                //  a.k.a. command line arguments
     int *indices;
     int counter;
 } Patterns;
@@ -53,6 +54,20 @@ void print_line_number(int number) {
     printf("%6d  ", number);                                                                                //  width for line number is 6
 }
 
+int are_equal(const char* string1, const char* string2, int length) {
+    for (int index = 0; index < length; ++index)
+        if (string1[index] != string2[index])
+            return False;
+    return True;
+}
+
+int is_found(const char* substring, int sublength, const char* string, int length) {
+    for (int index = 0; index < length - sublength; ++index)
+        if (are_equal(substring, string + index, sublength))
+            return True;
+    return False;
+}
+
 void print_line(int *line_number, const char* line, int length, const Flags* flags, int* is_empty) {
     // if (flags->s && *is_empty && length <= 1)
     //     return;
@@ -88,6 +103,31 @@ void print_line(int *line_number, const char* line, int length, const Flags* fla
     UNUSED_SHIT(line_number);
 }
 
+int get_string_length(const char* string) {
+    int length = 0;
+    while (string[length])
+        ++length;
+    return length;
+}
+
+int is_line_suitable(const char* line, int line_length, const Flags* flags, const Patterns* patterns) {
+    int is_suitable = False;
+        
+    for (int pattern_number = 0; pattern_number < patterns->counter; ++pattern_number) {
+        const char* word = patterns->words[patterns->indices[pattern_number]];
+        const int word_length = get_string_length(word);
+
+        if (is_found(word, word_length, line, line_length)) {
+            is_suitable = True;
+            break;
+        }
+    }
+
+
+    UNUSED_SHIT(flags);
+    return is_suitable;
+}
+
 void read_and_output_file_line_by_line(const char* filename, const Flags* flags, const Patterns* patterns) {
     FILE* input_file = fopen(filename, "r");
     if (input_file == NULL) {
@@ -113,7 +153,8 @@ void read_and_output_file_line_by_line(const char* filename, const Flags* flags,
             break;
         }
         // line_actual_length = get_line_length(line);
-        print_line(&line_number, line, line_actual_length, flags, &is_line_empty);
+        if (is_line_suitable(line, line_actual_length, flags, patterns))
+            print_line(&line_number, line, line_actual_length, flags, &is_line_empty);
     }
 
     free(line);                                                                                             //  because getline allocates memory
@@ -126,19 +167,7 @@ void read_and_output_file_line_by_line(const char* filename, const Flags* flags,
     UNUSED_SHIT(patterns);
 }
 
-int are_equal(const char* string1, const char* string2, int length) {
-    for (int index = 0; index < length; ++index)
-        if (string1[index] != string2[index])
-            return False;
-    return True;
-}
 
-int is_found(const char* substring, int sublength, const char* string, int length) {
-    for (int index = 0; index < length - sublength; ++index)
-        if (are_equal(substring, string + index, length))
-            return True;
-    return False;
-}
 
 void print_command_line_arguments(int counter, const char** arguments) {
     for (int index = 0; index < counter; ++index) {
@@ -146,14 +175,9 @@ void print_command_line_arguments(int counter, const char** arguments) {
     }
 }
 
-int get_string_length(const char* string) {
-    int length = 0;
-    while (string[length])
-        ++length;
-    return length;
-}
 
-void set_flags(int counter, const char** arguments, Flags* flags, int* flag_counter, Patterns* patterns) {
+
+void set_flags(int counter, const char** arguments, Flags* flags, int* words_counter, Patterns* patterns) {
     static const int short_flag_length = 2;
 
     int pattern_index = 0;
@@ -165,59 +189,62 @@ void set_flags(int counter, const char** arguments, Flags* flags, int* flag_coun
             are_equal("-e", arguments[argument_index], argument_length)) {
 
             flags->e = True;
-            ++(*flag_counter);
+            (*words_counter) += 2;
 
-            patterns->indices[pattern_index] = argument_index + 1;  //  because pattern goes right after -e, so its index is +1
+            printf("pattern #%d: %s\n", argument_index + 1, arguments[argument_index + 1]);
+            patterns->indices[pattern_index] = argument_index + 1 + 1;  //  because pattern goes right after -e, so its index is +1
+            printf("pattern #%d: %s\n", patterns->indices[pattern_index], patterns->words[patterns->indices[pattern_index] + 1]);
             ++pattern_index;
             ++(patterns->counter);
+
 
         } else if (argument_length == short_flag_length && 
                    are_equal("-i", arguments[argument_index], argument_length)) {
 
             flags->i = True;
-            ++(*flag_counter);
+            ++(*words_counter);
 
         } else if (argument_length == short_flag_length && 
                    are_equal("-v", arguments[argument_index], argument_length)) {
 
             flags->v = True;
-            ++(*flag_counter);
+            ++(*words_counter);
         
         } else if (argument_length == short_flag_length && 
                    are_equal("-c", arguments[argument_index], argument_length)) {
 
             flags->c = True;
-            ++(*flag_counter);
+            ++(*words_counter);
         
         } else if (argument_length == short_flag_length && 
                    are_equal("-l", arguments[argument_index], argument_length)) {
 
             flags->l = True;
-            ++(*flag_counter);
+            ++(*words_counter);
         
         } else if (argument_length == short_flag_length && 
                    are_equal("-n", arguments[argument_index], argument_length)) {
 
             flags->n = True;
-            ++(*flag_counter);
+            ++(*words_counter);
         
         } else if (argument_length == short_flag_length && 
                    are_equal("-h", arguments[argument_index], argument_length)) {
 
             flags->h = True;
-            ++(*flag_counter);
+            ++(*words_counter);
         
         } else if (argument_length == short_flag_length && 
                    are_equal("-s", arguments[argument_index], argument_length)) {
 
             flags->s = True;
-            ++(*flag_counter);
+            ++(*words_counter);
         
         } else if (argument_length == short_flag_length && 
                    are_equal("-f", arguments[argument_index], argument_length)) {
 
             flags->f = True;
-            ++(*flag_counter);
+            ++(*words_counter);
 
             assert(0 && "NOT IMPLEMENTED FILENAME STORAGE FOR REGEX!");
             //  TODO: store somewhere filename for regex!!!
@@ -226,6 +253,7 @@ void set_flags(int counter, const char** arguments, Flags* flags, int* flag_coun
 }
 
 void initialize_patterns(Patterns* patterns) {
+    patterns->words = NULL;
     patterns->indices = NULL;
     patterns->counter = 0;
 }
@@ -242,12 +270,14 @@ int main(int counter, const char **arguments) {
     Patterns patterns;
     initialize_patterns(&patterns);
     patterns.indices = malloc(counter * sizeof(int));
+    patterns.words = arguments;
 
 
     int flag_counter = 0;
     set_flags(counter - 1, arguments + 1, &flags, &flag_counter, &patterns);
 
     for (int file_index = flag_counter + 1; file_index < counter; ++file_index) {
+        printf("filename #%d: %s\n", file_index, arguments[file_index]);
         read_and_output_file_line_by_line(arguments[file_index], &flags, &patterns);
     }
     UNUSED_SHIT(counter);
